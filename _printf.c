4ff808eb99bfbb2d5293c42a8dbc04c47981b4ab
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-05 21:02:40 -0400

    Re-factor code

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdarg.h>
 #include <string.h>
 /**
@@ -10,48 +11,53 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j = 0, len = 0;
+	int i = 0, j = 0, len = 0, find;
 	print_type types[] = {
 		{"c", _print_chr}, {"s", _print_str}, {"d", _print_num}, {"i", _print_num},
 		{"X", _print_hxl}, {"x", _print_hxs}, {"o", _print_oct}, {NULL, NULL} };
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
-	while (format[i])
+
+	len = 0;
+
+	for (i = 0; *(format + i); i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (*(format + i) == '%')
 		{
-			if (format[i + 1] == '%')
+			if (*(format + i + 1) == '\0')
+				continue;
+			find = 0;
+
+			for (j = 0; j < 6; j++)
 			{
-				_putchar('%');
-				len++;
-				i++;
-			}
-			else
-			{
-				j = 0;
-				while (types[j].type)
+				if (format[i + 1] == *types[j].type)
 				{
-					if (format[i + 1] == *types[j].type)
-					{
-						len += types[j].f(args);
-						i++;
-						break;
-					}
-					j++;
-				}
-				if (types[j].type == NULL)
-				{
-					_putchar(format[i]);
-					len++;
+					len += types[j].f(args);
+					find = 1;
+					format++;
+					break;
 				}
 			}
+
+			if (find != 1)
+			{
+				if (*(format + i + 1) == '%')
+				{
+					len += write(1, "%", 1);
+					format++;
+				}
+				else
+					len += write(1, (format + i), 1);
+			}
+
 		}
 		else
 		{
-			_putchar(format[i]);
-			len++;
+			len += write(1, (format + i), 1);
 		}
-		i++;
 	}
 	va_end(args);
 	return (len);
